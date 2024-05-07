4f1b119786cfac1f28dc31e88807470b4eae4303
Kleuber Silva <kleuber20@hotmail.com>
2024-05-07 10:49:15 -0300

    Add global styles, navigation, and tailwindcss-animate plugin
    This commit adds global styles for the app, including a navigation bar with sign-in and sign-up links. It also includes the tailwindcss-animate plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="min-h-screen w-full">
+      <nav className="w-full h-16 border-b border-border">
+        <div className="w-[80dvw] h-full flex justify-between mx-auto items-center">
+          <div>
+            <Link href="" className="text-2xl font-bold font-mono group flex gap-3 items-center">
+              Quiz
+              <span className="bg-primary p-2 rounded-sm text-accent group-hover:bg-secondary group-hover:text-accent-foreground transition-all duration-300">UP</span>
+            </Link>
+          </div>
+          <div className="flex flex-row gap-5">
+            <Link href="/auth/signin" className="text-base font-medium font-mono group flex gap-3 items-center text-accent bg-primary p-2 rounded-sm border hover:bg-transparent hover:text-accent-foreground transition-all duration-300 hover:border-primary">
+              Sign In
+            </Link>
+            <Link href="/auth/signup" className="text-base font-medium font-mono group flex gap-3 items-center p-2 rounded-sm border border-primary hover:bg-primary hover:text-accent transition-all duration-300">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
