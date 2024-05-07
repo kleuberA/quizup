9361bacce4beed0b65fc78894903a7df0131de31
Kleuber Silva <kleuber20@hotmail.com>
2024-05-07 11:40:19 -0300

    Add welcome message section to homepage
    This commit adds a new section to the homepage that displays a welcome message to the user. The section includes a title, a subtitle, and a description, all with appropriate styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,13 @@ export default function Home() {
           </div>
         </div>
       </nav>
+      <div className="bg-background w-full">
+        <div className="w-[80dvw] min-h-[calc(100dvh_-_4rem)] h-full mx-auto flex flex-col justify-center items-center">
+          <h1 className="text-lg lg:text-4xl font-bold text-accent-foreground">Welcome to QuizUP</h1>
+          <p className="text-sm lg:text-xl tracking-wider font-mono text-muted-foreground">Test your knowledge, always have fun!</p>
+          <p className="text-sm lg:text-lg text-muted-foreground">The best quiz platform for you.</p>
+        </div>
+      </div>
     </div>
   );
 }
