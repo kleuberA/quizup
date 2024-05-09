433d25f5661db2f6bb76265191f6d0f4a076017d
Kleuber Silva <kleuber20@hotmail.com>
2024-05-09 14:18:45 -0300

    "Add forgot password page with email input and submit button"
    This commit adds a new implementation for the ForgotPassword page. It includes a card with a title, description, email input field, and a submit button for forgetting a password

--- src/app/auth/forgotpassword/page.tsx
+++ src/app/auth/forgotpassword/page.tsx
@@ -1,5 +1,35 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function ForgotPassword() {
     return (
-        <div>forgot</div>
+        <div className="w-full min-h-screen flex flex-row items-center justify-center">
+            <Card className="mx-auto max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Forgot Your Password</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
+                        </div>
+                        <Button type="submit" className="w-full">
+                            Forgot Password
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
