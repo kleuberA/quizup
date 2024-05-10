007506e1ccc67e10a541d3e7644f5700a904d927
Kleuber Silva <kleuber20@hotmail.com>
2024-05-10 10:41:17 -0300

    Add ThemeProvider from next-themes to enable dynamic theme switching
    This commit adds the `ThemeProvider` component from `next-themes` to enable dynamic theme switching in the application. The `ThemeProvider` is used to wrap the

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,9 @@
+import Menu from "@/components/menu/Menu";
+
+export default function Quiz() {
+    return (
+        <div>
+            <Menu />
+        </div>
+    )
+}
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { ToggleTheme } from "../toggle-theme";
+
+export default function Menu() {
+    return (
+        <nav className="w-full h-16 border-b border-border">
+            <div className="w-[80dvw] h-full flex justify-between mx-auto items-center">
+                <div>
+                    <Link href="" className="text-2xl font-bold font-mono group flex gap-3 items-center">
+                        Quiz
+                        <span className="bg-primary p-2 rounded-sm text-accent group-hover:bg-secondary group-hover:text-accent-foreground transition-all duration-300">UP</span>
+                    </Link>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <ToggleTheme />
+                </div>
+            </div>
+        </nav>
+    )
+}
