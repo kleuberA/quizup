0ea26089b4c666ffd5f27f9d90c3680979bf5b7e
Kleuber Silva <kleuber20@hotmail.com>
2024-05-12 11:00:38 -0300

    "Updated Question component styling and added conditional classes to question options"
    This commit updates the styling of the Question component by removing the background color from the container div and adding padding to the inner div. It also adds conditional classes

--- src/components/questions/Question.tsx
+++ src/components/questions/Question.tsx
@@ -31,13 +31,13 @@ const Question: React.FC<QuestionProps> = ({ question, options, answer, onNext, 
     };
 
     return (
-        <div className='bg-red-500 min-h-[90dvh] flex justify-center items-center w-full'>
-            <div className='w-[40dvw] bg-green-200 min-h-[60dvh] rounded-sm border border-border mx-auto gap-5 flex flex-col items-center justify-center'>
+        <div className='min-h-[90dvh] flex justify-center items-center w-full'>
+            <div className='w-[40dvw] min-h-[60dvh] p-4 rounded-sm border border-border mx-auto gap-5 flex flex-col items-center justify-center'>
                 <h1 className='text-start w-[32dvw] text-xl font-bold'>Questão - {currentIndex}</h1>
                 <h2>{question}</h2>
-                <ul className='w-[32dvw]'>
+                <ul className='w-[32dvw] flex flex-col gap-2'>
                     {Object.entries(options).map(([key, value]) => (
-                        <li key={key} className={`${isCorrect && selectedOption === key && 'bg-green-500'} ${!isCorrect && selectedOption === key && 'bg-red-500'}`}>
+                        <li key={key} className={`border-2 border-border text-accent-foreground p-2 rounded-md ${isCorrect && selectedOption === key && 'bg-green-500'} ${!isCorrect && selectedOption === key && 'bg-red-500'}`}>
                             <Label className='flex items-center flex-row gap-2'>
                                 <Input
                                     className='w-5 h-10'
@@ -53,7 +53,7 @@ const Question: React.FC<QuestionProps> = ({ question, options, answer, onNext, 
                     ))}
                 </ul>
                 {selectedOption && (
-                    <p>{isCorrect ? 'Resposta correta!' : 'Resposta incorreta.'}</p>
+                    <p className={`text-sm tracking-widest text-start w-[31dvw] ${isCorrect ? 'text-green-500' : 'text-red-500'} `}>{isCorrect ? 'Resposta correta!' : 'Resposta incorreta.'}</p>
                 )}
                 {isCorrect && !isCorrect && (
                     <p>Resposta correta: {options[answer]}</p>
